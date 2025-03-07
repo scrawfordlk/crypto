3eba6f26df544be4ccc2d673e1b0a2a246b10ceb
Sam Crawford <sam.crawford@stud.plus.ac.at>
2025-03-07 01:11:43 +0100

    possibly correct implementation?

--- 1/1/bakers_map.py
+++ 1/1/bakers_map.py
@@ -12,43 +12,37 @@ def main():
     cv2.imshow("Baker's Map Encryption", gray_image)
     cv2.waitKey(0)
 
-    print("Entropy of initial image: " + str(measure_entropy(gray_image)))
+    print_entropy(gray_image)
 
-    i = 1
+    encrypted_image = gray_image.copy()
     while True:
-        encrypted_image = encrypt_image(gray_image, i)
-        print("New entropy: " + str(measure_entropy(encrypted_image)))
+        encrypted_image = encrypt_image(encrypted_image)
+        print_entropy(encrypted_image)
         cv2.imshow("Baker's Map Encryption", encrypted_image)
         key = cv2.waitKey(0)
         if key == ord("n"):
-            i += 1
-        elif key == ord("b"):
-            i -= 1
+            continue
         elif key == ord("q"):
             cv2.destroyAllWindows()
             exit()
 
 
-def encrypt_image(image, iterations=1):
+def encrypt_image(image):
     length, _ = image.shape
-    encrypted_image = image.copy()
+    encrypted_img = image.copy()
 
-    for i in range(iterations):
-        temp_img = encrypted_image.copy()
+    for x in range(length):
+        for y in range(length):
+            if x < length / 2:
+                encrypted_img[x][y] = image[2 * x + y % 2][y // 2]
+            else:
+                encrypted_img[x][y] = image[2 * x - length + y % 2][(y + length) // 2]
 
-        for x in range(length):
-            for y in range(length):
-                if x < length / 2:
-                    temp_img[x][y] = encrypted_image[2 * x][y // 2]
-                else:
-                    temp_img[x][y] = encrypted_image[2 * x - length][(y + length) // 2]
-        encrypted_image = temp_img
-
-    return encrypted_image
+    return encrypted_img
 
 
-def measure_entropy(image):
-    return skimage.measure.shannon_entropy(image)
+def print_entropy(image):
+    return print("Entropy: " + str(skimage.measure.shannon_entropy(image)))
 
 
 if __name__ == "__main__":
