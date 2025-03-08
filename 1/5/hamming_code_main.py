d4b5302e35670374ae0164dffd3b5c20736f8ef8
Sam Crawford <sam.crawford@stud.plus.ac.at>
2025-03-08 22:47:05 +0100

    added hamming lib and main using it

---
+++ 1/5/hamming_code_main.py
@@ -0,0 +1,27 @@
+import hamming
+from bitarray import bitarray
+
+
+def main():
+    msg = bitarray("1101")
+    print(f"Original Bits: {msg}")
+    print()
+
+    encoded_msg = hamming.encode(msg)
+    print(f"Encoded Bits: {encoded_msg}")
+
+    decoded_msg = hamming.decode(encoded_msg)
+    print(f"Decoded Bits: {decoded_msg}")
+    print()
+
+    # simulating corruption
+    corrupt_encoded_msg = encoded_msg.copy()
+    corrupt_encoded_msg[6] = corrupt_encoded_msg[6] ^ 1
+    print(f"Corrupted encoded Bits: {corrupt_encoded_msg}")
+
+    corrupt_decoded_msg = hamming.decode(corrupt_encoded_msg)
+    print(f"Decoded Bits: {corrupt_decoded_msg}")
+
+
+if __name__ == "__main__":
+    main()
